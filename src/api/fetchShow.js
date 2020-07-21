4fa10f0bc7efdc9cc0a3327c7197b7c6971233ed
Cameron Lares <cameronlares@gmail.com>
2020-07-20 23:25:50 -0400

    Attempt MVP

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,29 +1,13 @@
-import React from 'react'
-import axios from 'axios'
-import {useEffect,useState} from 'react'
+import axios from "axios";
 
-// fetchShow.js
 export const fetchShow = () => {
-    return axios.get("")
-      .then(res => {
-        console.log(res)  
-       return res // or res.data, however you want to set that up
-  })
-  .catch(err => {
-    console.error("error fetching data from api, err: ", err.message);
-    return err;
-  });
-}
-    
-   // App.js
-  useEffect(() => {
-    fetchShow
-.then(res => {
-        // set state with the data
-        const [state, setState] =useState("")
-      })
-      .catch(err => {
-        console.error("error fetching data from api, err: ", err.message);
-        return err;
-      });
-    }, []);
+  return axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    ).then((res) => {
+      return res
+    })
+    .catch((err) => {
+      return err
+    });
+};
